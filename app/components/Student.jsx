d0c3a033b5ee38d18e9755e7dd8f0ad478b6647d
Wasif Zaman <teatocode@gmail.com>
2017-09-22 19:50:35 -0400

    react reduxx student

--- app/components/Student.jsx
+++ app/components/Student.jsx
@@ -1,37 +1,29 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import store from '../store'
 import { fetchAllStudents } from '../reducers'
 
-export default class Student extends Component {
-  constructor() {
-    super()
-    this.state = store.getState()
-  }
+const Student = (props)=> {
+  const student = props.state.students.find(s=> s.id == props.match.params.id*1)
 
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(()=> {
-      this.setState(store.getState)
-    })
+  if (!student) return <div></div>
+  return (
+    <div>
+      <p>{ student.name }</p>
+      { student.campus ?
+        <Link to={ `/campus/${ student.campus.id }` }>{ student.campus.name }</Link> : null
+      }
+    </div>
+  )
+}
 
-    store.dispatch(fetchAllStudents())
-  }
+store.dispatch(fetchAllStudents())
 
-  componentWillUnmount() {
-    this.unsubscribe()
-  }
-
-  render() {
-    const student = this.state.students.find(s=> s.id == this.props.match.params.id*1)
-    
-    if (!student) return <div></div>
-    return (
-      <div>
-        <p>{ student.name }</p>
-        { student.campus ?
-          <Link to={ `/campus/${ student.campus.id }` }>{ student.campus.name }</Link> : null
-        }
-      </div>
-    )
+const mapStateToProps = (state)=> {
+  return {
+    state
   }
 }
+
+export default connect(mapStateToProps)(Student)
