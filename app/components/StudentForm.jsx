c723bc863da39c79cf441dc577be29b7f6e52175
Wasif Zaman <teatocode@gmail.com>
2017-09-24 02:59:40 -0400

    clean :)

--- app/components/StudentForm.jsx
+++ app/components/StudentForm.jsx
@@ -1,26 +1,40 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { writeStudentName } from '../reducers'
+import { writeStudentName, fetchStudent, resetState } from '../reducers'
 
-const StudentForm = ({ onChangeHandler }) => {  
-  return (
-    <form>
-      <div>
-        <label htmlFor='name'>Name</label>
-        <input name='name' onChange={ onChangeHandler }/>
-      </div>
-    </form>
-  )
+class StudentForm extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params
+    id ? this.props.getStudent(id) : this.props.resetForm()
+  }
+
+  render() {
+    const { student, onChangeHandler } = this.props
+    return (
+      <form>
+        <div>
+          <label htmlFor='name'>Name</label>
+          <input name='name' value={ student.name } onChange={ onChangeHandler }/>
+        </div>
+      </form>
+    )
+  }
 }
 
-const mapStateToProps = () => {
-  return { }
+const mapStateToProps = ({ student }) => {
+  return { student }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
     onChangeHandler(ev) {
       dispatch(writeStudentName(ev.target.value))
+    },
+    getStudent(id) {
+      dispatch(fetchStudent(id))
+    },
+    resetForm() {
+      dispatch(resetState())
     }
   }
 }
--- app/reducers/student.jsx
+++ app/reducers/student.jsx
@@ -1,8 +1,12 @@
 import axios from 'axios'
+import campuses from './campuses'
+import students from './students'
 
 // ACTION NAMES
 
 const WRITE_STUDENT_NAME = 'WRITE_STUDENT_NAME'
+const SET_STUDENT_CAMPUS = 'SET_STUDENT_CAMPUS'
+const RESET_STATE = 'RESET_STATE'
 
 // ACTION CREATORS
 export const writeStudentName = name => {
@@ -12,18 +16,43 @@ export const writeStudentName = name => {
   }
 }
 
+export const setStudentCampus = campusId => {
+  return {
+    type: SET_STUDENT_CAMPUS,
+    campusId
+  }
+}
+
+export const resetState = () => {
+  return {
+    type: RESET_STATE
+  }
+}
+
 // THUNK
+export const fetchStudent = id => dispatch => {
+  axios.get(`/api/students/${id}`)
+    .then(response=> response.data)
+    .then(student=> {
+      dispatch(writeStudentName(student.name))
+      dispatch(setStudentCampus(student.campusId))
+    })
+}
 
 // REDUCERS
 const initialState = {
   name: '',
-  campusId: null
+  campusId: 0 
 }
 
 export default function reducer (student = initialState, action) {
   switch (action.type) {
     case WRITE_STUDENT_NAME:
       return { ...student, name: action.name }
+    case SET_STUDENT_CAMPUS:
+      return { ...student, campusId: action.campusId }
+    case RESET_STATE:
+      return initialState 
     default:
       return student 
   }
