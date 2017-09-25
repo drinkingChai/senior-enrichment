a2b2c2a938b012f7c924fe6845c2e1b812336ccd
Wasif Zaman <teatocode@gmail.com>
2017-09-25 09:46:54 -0400

    seed

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -2,29 +2,14 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
 import store from '../store'
 import { fetchCampuses, fetchStudents } from '../reducers' 
-import Nav from './Nav'
-import Campuses from './Campuses'
-import CampusForm from './CampusForm'
-import Students from './Students'
-import StudentForm from './StudentForm'
 
 export default class Root extends Component {
   componentDidMount() {
-    store.dispatch(fetchCampuses())
-    store.dispatch(fetchStudents())
   }
 
   render() {
     return (
       <div>
-        <Nav />
-
-        <Route exact path='/campuses' component={ Campuses }/>
-        <Route exact path='/campuses/:id' component={ CampusForm }/>
-        <Route exact path='/add-campus' component={ CampusForm }/>
-        <Route exact path='/students' component={ Students }/>
-        <Route exact path='/students/:id' component={ StudentForm }/>
-        <Route exact path='/add-student' component={ StudentForm }/>
       </div>
     )
   }
--- db/seed.js
+++ db/seed.js
@@ -1,27 +1,35 @@
+const db = require('./index')
 const Campus = require('./models/Campus')
 const Student = require('./models/Student')
 
 const seed = ()=> {
-  return Promise.all([
-    Campus.create({ name: 'Luna' }),
-    Campus.create({ name: 'Terra' }),
-    Campus.create({ name: 'Mars' }),
-    Campus.create({ name: 'Titan' }),
-    Student.create({ name: 'Gabe', email: 'gabe@iheartjupiter.com' }),
-    Student.create({ name: 'Ashi', email: 'ashi@iheartjupiter.com' }),
-    Student.create({ name: 'Dan', email: 'dan@earthishome.com' }),
-    Student.create({ name: 'Marvin', email: 'mars@godofwar.com' }),
-    Student.create({ name: 'Stinky', email: 'stinky@moonbase.com' })
-  ])
-  .then(([luna, terra, mars, titan, gabe, ashi, dan, marvin, stinky])=> {
-    return Promise.all([
-      titan.addStudent(gabe),
-      titan.addStudent(ashi),
-      terra.addStudent(dan),
-      mars.addStudent(marvin),
-      luna.addStudent(stinky)
-    ])
-  })
+  db.sync({ force: true })
+    .then(()=> {
+      return Promise.all([
+        Campus.create({ name: 'Luna' }),
+        Campus.create({ name: 'Terra' }),
+        Campus.create({ name: 'Mars' }),
+        Campus.create({ name: 'Titan' }),
+        Student.create({ name: 'Gabe', email: 'gabe@iheartjupiter.com' }),
+        Student.create({ name: 'Ashi', email: 'ashi@iheartjupiter.com' }),
+        Student.create({ name: 'Dan', email: 'dan@earthishome.com' }),
+        Student.create({ name: 'Marvin', email: 'mars@godofwar.com' }),
+        Student.create({ name: 'Stinky', email: 'stinky@moonbase.com' })
+      ])
+    })
+    .then(([luna, terra, mars, titan, gabe, ashi, dan, marvin, stinky])=> {
+      return Promise.all([
+        titan.addStudent(gabe),
+        titan.addStudent(ashi),
+        terra.addStudent(dan),
+        mars.addStudent(marvin),
+        luna.addStudent(stinky)
+      ])
+    })
+    .then(()=> { 
+      console.log('database seeded')
+      db.close()
+    })
 }
 
-module.exports = seed
+seed()
